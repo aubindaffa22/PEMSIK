a81d06a0a88b5203c94831a103e2e5a894e975b3
Aubindaffaalghozy <aubindaffaa321@gmail.com>
2026-04-24 00:21:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+//import Login from "./Pages/Login/Login";
+import Mahasiswa from "./Pages/Admin/Mahasiswa/Mahasiswa";
+
+const App = () => {
+    // return <Login />
+    return <Mahasiswa />
+}
+
+export default App;
---
+++ src/Data/Dummy.js
@@ -0,0 +1,22 @@
+// Dummy user untuk login
+export const dummyUser = {
+  email: "admin@mail.com",
+  password: "123456",
+  name: "Admin",
+};
+
+// Dummy data mahasiswa
+export const mahasiswaList = [
+  {
+    nim: "A11.2023.15324",
+    nama: "Aubin Daffa Al Ghozy",
+  },
+  {
+    nim: "20211002",
+    nama: "Siti Aminah",
+  },
+  {
+    nim: "20211003",
+    nama: "Ahmad Fauzi",
+  },
+];
